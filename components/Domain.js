f42094f8ebb510936115681952f5b749298ff238
officialzoka <golldenhouse@gmail.com>
2022-08-27 15:50:27 +0200

    social icons bar

--- components/Domain.js
+++ components/Domain.js
@@ -1,19 +1,24 @@
 import React from "react";
 import Xmark from "@ui/icons/Xmark";
 import Check from "@ui/icons/Check";
+import clsx from "clsx";
 
-export default function Domain({domain,exists}) {
+export default function Domain({ domain, exists }) {
 	return (
-		<div
-			className="bg-zinc-800 mb-2 p-2.5 rounded-lg flex flex-row items-center justify-between"
-		>
-			<h2 className={`text-lg font-medium ${exists && "text-white/50"}`}>
+		<div className="bg-zinc-800 mb-2 p-2.5 rounded-lg flex flex-row items-center justify-between">
+			<h2
+				className={clsx(
+					exists && "text-white/50",
+					"text-lg font-medium"
+				)}
+			>
 				{domain}
 			</h2>
 			<span
-				className={`p-0.5 rounded-full ${
-					exists ? "bg-amber-900" : "bg-lime-600"
-				}`}
+				className={clsx(
+					exists ? "bg-amber-900" : "bg-lime-600",
+					"p-0.5 rounded-full"
+				)}
 			>
 				{exists ? <Xmark /> : <Check />}
 			</span>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
-import Form from "@ui/Form"
-import Header from "@ui/Header"
-import DomainList from "@ui/DomainList"
+import Form from "@ui/Form";
+import Header from "@ui/Header";
+import DomainList from "@ui/DomainList";
+import SocialBar from "@ui/SocialBar";
 
 export default function Home() {
-	
 	return (
 		<>
 			<Head>
@@ -16,10 +16,11 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<div className="text-zinc-100 conatiner mx-auto px-4 md:px-32">
-				<Header/>
-				<Form/>
-				<DomainList/>
+				<Header />
+				<Form />
+				<DomainList />
 			</div>
+			<SocialBar />
 		</>
 	);
 }
